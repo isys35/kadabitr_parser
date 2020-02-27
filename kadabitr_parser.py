9a10de6344ec93d4fa0e7c7f3398059d0bd782bc
isys35 <isys35@mail.ru>
2020-02-27 12:20:29 +0300

    Получение данных со страницы

--- kadabitr_parser.py
+++ kadabitr_parser.py
@@ -6,37 +6,6 @@ from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup as BS
 
 
-# headers_main= {
-#     'Host': 'kad.arbitr.ru',
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
-#     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-#     'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-#     'Accept-Encoding': 'gzip, deflate',
-#     'Connection': 'keep-alive',
-#     'Upgrade-Insecure-Requests': '1',
-#     'Cache-Control': 'max-age=0'
-# }
-# url = 'http://kad.arbitr.ru/Kad/SearchInstances'
-# affairs_html_list = []
-# for i in range(1, 41):
-#     time.sleep(6)
-#     data ='{"Page":1,"Count":25,"CaseType":"G","Courts":[],"DateFrom":null,"DateTo":null,"Sides":[],"Judges":[],"CaseNumbers":[],"WithVKSInstances":false}'
-#     response = None
-#     while not response:
-#         try:
-#             response = requests.post(url, headers=headers_search, data=data.encode('utf8'))
-#         except requests.exceptions.ConnectionError:
-#             print('[WARNING] Проблема с подключением')
-#             time.sleep(30)
-#             print('[INFO] Попытка подключения')
-#     if response.status_code == 200:
-#         affairs_html_list.append(response.text)
-#     else:
-#         print('[WARNING] Ошибка ' + str(response.status_code))
-#         wasm = input('wasm = :')
-#         time.sleep(5)
-
-
 class ParserKadabitr:
     def __init__(self):
         self.search_url = 'http://kad.arbitr.ru/Kad/SearchInstances'
@@ -94,21 +63,10 @@ class ParserKadabitr:
 
     def get_elements_in_data(self):
         time.sleep(5)
-        data_json = self.get_data_json(1, self.date_from, self.date_to)
-        self.update_headers(self.wasm)
-        response = None
-        while not response:
-            try:
-                response = requests.post(self.search_url, headers=self.headers_search, data=data_json.encode('utf8'))
-            except Exception as ex:
-                print(ex)
-                print('[WARNING] Проблема с подключением')
-                time.sleep(30)
-                print('[INFO] Попытка подключения')
-            if response.status_code == 200:
-                soup = BS(response.text, 'html.parser')
-                count_affairs = soup.select('#documentsTotalCount')[0]['value']
-                return count_affairs
+        response_txt = self.response_text(1)
+        soup = BS(response_txt, 'html.parser')
+        count_affairs = soup.select('#documentsTotalCount')[0]['value']
+        return count_affairs
 
     def get_data_low(self):
         count_affairs = int(self.get_elements_in_data())
@@ -119,7 +77,72 @@ class ParserKadabitr:
         page = count_affairs//25
         print('[INFO] Количество страниц ' + str(page))
 
+    def response_text(self, page):
+        data_json = self.get_data_json(page, self.date_from, self.date_to)
+        self.update_headers(self.wasm)
+        response = None
+        while not response:
+            try:
+                response = requests.post(self.search_url, headers=self.headers_search, data=data_json.encode('utf8'))
+                print(response.status_code)
+            except Exception as ex:
+                print(ex)
+                print('[WARNING] Проблема с подключением')
+                time.sleep(30)
+                print('[INFO] Попытка подключения')
+            if response.status_code == 200:
+                return response.text
+
+    def get_page_data(self, page):
+        time.sleep(5)
+        response_txt = self.response_text(page)
+        soup = BS(response_txt, 'html.parser')
+        containers = soup.select('.b-container')
+        all_split_containers = []
+        i = 0
+        data = []
+        while i != len(containers):
+            all_split_containers.append([containers[i], containers[i+1], containers[i+2], containers[i+3]])
+            i += 4
+        for affair in all_split_containers:
+            date = affair[0].select('.civil')
+            if date:
+                date = date[0].text.replace('\n', '')
+            else:
+                date = affair[0].select('.civil_simple')[0].text.replace('\n', '')
+            num = affair[0].select('.num_case')[0].text.replace('\n', '').strip()
+            href = affair[0].select('.num_case')[0]['href']
+            judge = affair[1].select('.judge')
+            if judge:
+                judge = affair[1].select('.judge')[0]['title']
+            else:
+                judge = None
+            instance = affair[1].select('div')[-1]['title']
+            plaintiff_name = affair[2].select('.js-rolloverHtml')[0].select('strong')[0].text
+            plaintiff_inn = affair[2].select('.js-rolloverHtml')[0].select('div')[0].text.replace('\n', '').strip()
+            plaintiff_adress = affair[2].select('.js-rolloverHtml')[0].text\
+                .replace(plaintiff_name, '').replace(plaintiff_inn,'').replace('\n', '').strip()
+            defendant_name = affair[3].select('.js-rolloverHtml')[0].select('strong')[0].text
+            defendant_inn = affair[3].select('.js-rolloverHtml')[0].select('div')[0].text.replace('\n', '').strip()
+            defendant_adress = affair[3].select('.js-rolloverHtml')[0].text \
+                .replace(defendant_name, '').replace(defendant_inn, '').replace('\n', '').strip()
+            affair_data = {
+                'date': date,
+                'num': num,
+                'href': href,
+                'judge': judge,
+                'instance': instance,
+                'plaintiff_name': plaintiff_name,
+                'plaintiff_inn': plaintiff_inn,
+                'plaintiff_adress': plaintiff_adress,
+                'defendant_name': defendant_name,
+                'defendant_inn': defendant_inn,
+                'defendant_adress': defendant_adress
+            }
+            data.append(affair_data)
+        return data
+
 
 
 parser = ParserKadabitr()
-print(parser.get_data_low())
+parser.get_page_data(1)
